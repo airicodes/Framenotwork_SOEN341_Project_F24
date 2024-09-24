a75461e49f2161639580521c3e8f54cbcc2b62b9
20Mathieu03 <mpvmathieu03@gmail.com>
2024-09-24 14:13:11 -0400

    Remove List component and change Navbar Design

--- peer-assessment-app/app/Instructor/page.tsx
+++ peer-assessment-app/app/Instructor/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from "react";
 import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 import { Dehaze } from "@mui/icons-material";
 import Box from "@mui/material/Box";
 import AccountMenu from "./components/AccountMenu";
@@ -9,21 +10,9 @@ import List from "./components/List";
 
 export default function Home() {
 
-    function openSidebar() {
-
-            const sidebar = document.getElementById("sidebar");
-            const navbar = document.getElementById("main-container");
-            const button = document.getElementById("openbtn");
-            if ( sidebar!== null && navbar !== null && button !== null){
-                    sidebar.style.width = "200px";
-                    navbar.style.marginLeft = "200px"
-                    button.style.visibility = "hidden";
-            }
-    }
-
     return(
         <>
-        <div id="main-container">
+        {/* <div id="main-container">
             <div style={{paddingTop: "10px", paddingBottom: "10px", paddingLeft: "10px", width: "100%", backgroundColor:"#111"}}>
                 <button id="openbtn" className="openbtn" onClick={openSidebar}><Dehaze/></button>
                 <AccountMenu></AccountMenu>
@@ -31,7 +20,8 @@ export default function Home() {
             <Box sx={{padding: "20px"}}>
                 <List />
             </Box>
-        </div>
+        </div> */}
+        <Navbar/>
         
         <Sidebar/>
         
