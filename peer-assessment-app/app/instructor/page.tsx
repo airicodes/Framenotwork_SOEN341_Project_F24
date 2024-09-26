0c17324c97e2a0bfe667ddb421f51a27ccb380d5
KapJ1coH <tima.mtl@gmail.com>
2024-09-26 16:04:39 -0400

    added template instructor and student pages

---
+++ peer-assessment-app/app/instructor/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InstructorPage: React.FC = () => {
+    return (
+        <div style={{ padding: '20px', textAlign: 'center' }}>
+            <h1>Instructor Page</h1>
+        </div>
+    );
+};
+
+export default InstructorPage;
