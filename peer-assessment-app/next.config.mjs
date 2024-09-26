5620a222fd48ee02d095cd496945c028ce406081
M-a-a-d-man <evanteboul@icloud.com>
2024-09-26 16:27:02 -0400

    reconfigured .env file

--- peer-assessment-app/next.config.mjs
+++ peer-assessment-app/next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        host_dev: 'http://localhost:3000',
-        port_dev: 3000,
-        user_dev: 'root',
-        password_dev: 'root',
-        database_dev: 'peer_assessment',
 
-    }
 };
 
 export default nextConfig;
