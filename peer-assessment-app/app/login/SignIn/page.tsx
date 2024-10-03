7ae89bf296a46912be83e40f0ea3b092ad6ea9f9
M-a-a-d-man <evanteboul@icloud.com>
2024-10-03 16:15:31 -0400

    added redirect for the signup page

--- peer-assessment-app/app/login/SignIn/page.tsx
+++ peer-assessment-app/app/login/SignIn/page.tsx
@@ -1,32 +1,6 @@
 import SignIn,{FormContainer} from "./SignIn";
-// import { useState } from "react";
-// import SignUp from "../Signup/Signup";
-// import Alert from '@mui/material/Alert';
-// import Snackbar, { SnackbarCloseReason } from '@mui/material/Snackbar';
-// import { useRouter } from "next/navigation";
+
 export default function Home() {
-  // const [isSignIn, setIsSignIn] = useState(true);
-  // const [openAlert, setOpenAlert] = useState(false);
-  // const handleClose = (
-  //   event?: React.SyntheticEvent | Event,
-  //   reason?: SnackbarCloseReason,
-  // ) => {
-  //   if (reason === 'clickaway') {
-  //     return;
-  //   }
-
-  //   setOpenAlert(false);
-  // };
-  // const router = useRouter();
-
-  // const toggleAuthMode = () => {
-  //   // setIsSignIn((prev) => !prev);
-  //   router.push('/login/Signup');
-  // };
-
- 
-
-
   return (
     <FormContainer>
       <SignIn/>
--- peer-assessment-app/app/login/Signup/page.tsx
+++ peer-assessment-app/app/login/Signup/page.tsx
@@ -1,55 +1,9 @@
-// 'use client';
 import {FormContainer} from "../SignIn/SignIn";
-// import { useState } from "react";
 import SignUp from "./Signup";
-// import Alert from '@mui/material/Alert';
-// import Snackbar, { SnackbarCloseReason } from '@mui/material/Snackbar';
-// import { useRouter } from "next/navigation";
-
 export default function Home() {
-  // const router = useRouter();
-  // const [isSignIn, setIsSignIn] = useState(true);
-  // const [openAlert, setOpenAlert] = useState(false);
-  // const handleClose = (
-  //   event?: React.SyntheticEvent | Event,
-  //   reason?: SnackbarCloseReason,
-  // ) => {
-  //   if (reason === 'clickaway') {
-  //     return;
-  //   }
-
-    // setOpenAlert(false);
-  // };
-  // const toggleAuthMode = () => {
-  //   // setIsSignIn((prev) => !prev);
-  //   router.push('/login/signup');
-  // };
-
-  // const handleOpenAlert = () => {
-  //   // setOpenAlert((prev) => !prev);
-  //   router.push('/login/SignIn');
-  //   // setIsSignIn((prev) => !prev);
-  // }
-
-
   return (
     <FormContainer>
       <SignUp/>
-      {/* <Snackbar
-        open={openAlert}
-        autoHideDuration={6000}
-        onClose={handleClose}
-        >
-        <Alert
-            onClose={handleClose}
-            severity="success"
-            variant="outlined"
-            sx={{ width: '100%' }}
-          >
-            Registered successfully
-        </Alert> */}
-        
-        {/* </Snackbar> */}
     </FormContainer>
   );
 }
